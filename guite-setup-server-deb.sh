a29c50eeba4eef3fc3780239ab0aa1c7a21b4ff3
GuiteHub <guillaume.p.chanut@gmail.com>
2024-01-22 22:41:09 +0100

    add nala

--- guite-setup-server-deb.sh
+++ guite-setup-server-deb.sh
@@ -4,6 +4,14 @@
 # Description: Script pour configurer un serveur Debian/Ubuntu. Installe les paquets nécessaires,
 #              met à jour le système et configure SSH pour empêcher la connexion en tant que root.
 
+# Vérifier si le système est basé sur Debian/Ubuntu
+if [ -f /etc/debian_version ]; then
+    echo "Système Debian/Ubuntu détecté. Continuation du script..."
+else
+    echo "Ce script est conçu pour Debian/Ubuntu. Arrêt du script."
+    exit 1
+fi
+
 # Inclure le fichier de configuration
 source guite-setup-server.conf
 
@@ -24,4 +32,13 @@ echo "Configuration de SSH..."
 sed -i 's/#PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
 systemctl restart sshd
 
-echo "Configuration terminée. Maintenant BMDTU"
+# Demander à l'utilisateur s'il veut redémarrer maintenant
+echo "Configuration terminée. Le système va redémarrer dans 5 secondes pour appliquer les changements."
+echo "Appuie sur Ctrl+C pour annuler le redémarrage."
+
+# Attendre 5 secondes avant de redémarrer
+sleep 5
+echo "BMDTU."
+
+# Redémarrer le système
+reboot
